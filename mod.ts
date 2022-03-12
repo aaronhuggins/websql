fdb6d9aa7edb6550a13c79079441a17a7e331db0
Aaron Huggins <aaron.huggins@gmail.com>
2022-03-11 18:53:50 -0600

    Change exports

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
-export { openDatabase, configureSQLiteDB } from './src/Database.ts'
+export { configureSQLiteDB } from './src/SQLiteDBCache.ts'
+export { openDatabase } from './src/Database.ts'
 export type { Database } from './src/Database.ts'
 export type { SQLResultSet } from './src/SQLResultSet.ts'
 export type { SQLResultSetRowList } from './src/SQLResultSetRowList.ts'
