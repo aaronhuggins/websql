7c2888bfaadb5c7d9e070af6aaa0206098068c9d
Aaron Huggins <aaron.huggins@gmail.com>
2022-03-11 18:53:33 -0600

    Add a caching mechanism - Use the same db reference by mode - Clean up all db refs on program close - Ensure in-memory db refs are not destroyed before program close

---
+++ src/SQLiteDBCache.ts
@@ -0,0 +1,66 @@
+import { SQLiteDB } from '../deps.ts'
+import type { SqliteOptions } from '../deps.ts'
+
+export type TxMode = Exclude<SqliteOptions['mode'], undefined>
+
+export class SQLiteDBCache {
+  #cache: Map<string, SQLiteDB>
+  #modes: Record<string, TxMode>
+  #options: SqliteOptions
+
+  constructor () {
+    this.#cache = new Map()
+    this.#modes = {}
+    this.#options = { memory: true }
+    addEventListener('unload', () => this.#close())
+  }
+
+  db (name: string, mode: TxMode): SQLiteDB {
+    mode = this.#options.memory ? 'create' : mode
+    const cached = this.#cache.get(name)
+
+    if (cached === undefined) {
+      return this.#make(name, mode)
+    } else if (this.#modes[name] !== mode) {
+      cached.close()
+      this.#cache.delete(name)
+      delete this.#modes[name]
+
+      return this.#make(name, mode)
+    }
+
+    return cached
+  }
+
+  configure (customOptions: SqliteOptions = {}): Readonly<SqliteOptions> {
+    this.#options = {
+      ...this.#options,
+      ...customOptions
+    }
+
+    return Object.freeze(structuredClone(this.#options) as SqliteOptions)
+  }
+
+  #make (name: string, mode: TxMode): SQLiteDB {
+    const newDb = new SQLiteDB(name, { ...this.#options, mode })
+
+    this.#cache.set(name, newDb)
+    this.#modes[name] = mode
+
+    return newDb
+  }
+
+  #close () {
+    for (const db of this.#cache.values()) {
+      db.close(true)
+    }
+  }
+}
+
+/** Global SQLite database cache to manage access mode and closing files on Deno unload. */
+export const cache = new SQLiteDBCache()
+
+/** Mutates module options for underlying SQLiteDB implementation, and returns an immutable clone of existing options. */
+export function configureSQLiteDB (customOptions: SqliteOptions): Readonly<SqliteOptions> {
+  return cache.configure(customOptions)
+}
