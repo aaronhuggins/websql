6f0c04173ece00e671fc76d9cbe1b61cc631d76c
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-21 10:35:41 -0500

    Update depenedencies to latest

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { parse } from "https://deno.land/std@0.135.0/path/mod.ts";
-export { DB as SQLiteDB } from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
-export type { SqliteOptions } from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
+export { parse } from "https://deno.land/std@0.136.0/path/mod.ts";
+export { DB as SQLiteDB } from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
+export type { SqliteOptions } from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
